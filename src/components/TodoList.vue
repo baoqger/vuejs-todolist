25f8432226e21fbf88217c4f8efe540336a15580
baoqger <baoqger@aliyun.com>
2017-06-26 15:07:18 +0800

    initial upload

---
+++ src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>Completed Task: {{todos.filter( todo => {return todo.done === true}).length }}</p>
+    <p>Pending Task: {{todos.filter( todo => {return todo.done === false }).length }}</p>
+    <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo"></todo>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+import Todo from './Todo'
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo
+  },
+  methods: {
+    deleteTodo (todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1)
+    }
+  }
+}
+</script>
+<style>
+p.tasks {
+  text-align: center;
+}
+</style>
